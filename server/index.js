15263df729088f3f3040249feb5604230bf03b47
Anderson Fernandes <andersonfspell.dev@gmail.com>
2024-09-01 19:52:06 -0300

    update

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
-const express = require("require")
+const express = require("express")
 const app = express()
+const mysql = express()
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "",
+})
+
 
 app.listen(3001, () => {
   console.log("Rodando na porta 3001")
